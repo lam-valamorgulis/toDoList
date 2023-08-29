e83c8520df0ff016d05d37dabbb1a949546451dd
lam-valamorgulis <80387108+lam-valamorgulis@users.noreply.github.com>
2023-08-29 17:53:28 +0000

    1st commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,13 @@
 const Form = () => {
   return (
-    <form>
-      <h3>Add item </h3>
+    <form className="add-form">
+      <h3>Add item</h3>
       <input
         type="text"
         placeholder="Item..."
         value="test"
       />
-      <button>Add</button>
-      
-      
+      <button>Add</button>      
     </form>
   )
 }
